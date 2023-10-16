44c9d2e445d429693ee7f686b74f269c31567108
Ibrahim Ibrahimli <dev.ibrahimlii@gmail.com>
2023-10-16 22:46:57 +0400

    FizzBuzz Solving HackerRank

---
+++ FizzBuzz Problem Solving/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzBuzz_Problem_Solving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void fizzBuzz(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 3 == 0 && i % 15 != 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (i % 5 == 0 && i % 15 != 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else if (i % 15 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
